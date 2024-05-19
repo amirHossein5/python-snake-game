c53379c4a4087ff52d1edaaa6adf45074b0c597c
amirHossein5 <amir5665@yahoo.com>
2024-05-19 23:58:35 +0330

    init - empty screen

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+
+pygame.init()
+
+SW, SH = 800, 800
+BLOCK_SIZE = 50
+
+# FONT = pygame.font.Font('font.ttf', 100)
+
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Snake!')
+clock = pygame.time.Clock()
+
+
+def draw_grid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, '#3c3c3b', rect, 1)
+
+
+draw_grid()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(10)
